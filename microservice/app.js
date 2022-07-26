5ce0bec9249961dd61685beaa182797388aee222
William Garcia <willgrod@amazon.com>
2022-07-26 15:33:29 +1000

    chore: Set color to gray

--- microservice/app.js
+++ microservice/app.js
@@ -26,7 +26,7 @@ function getRandomColor() {
 function getRandomGrayScaleColor() {
   let value = Math.random() * 0xFF | 0,
       grayscale = (value << 16) | (value << 8) | value;
-  return '#' + grayscale.toString(16);
+  return '#' + grayscale.toString(18);
 }
 
 app.get('/hpa', function(request, response){
